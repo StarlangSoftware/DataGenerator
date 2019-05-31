b516592966449c9aab34533462b8211f8c74603d
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2019-05-31 08:16:52 +0300

    Corrected file names of the gazetteers.

--- src/main/java/DataGenerator/Attribute/IsLocationGazetteer.java
+++ src/main/java/DataGenerator/Attribute/IsLocationGazetteer.java
@@ -4,7 +4,7 @@ import Classification.Attribute.BinaryAttribute;
 import NamedEntityRecognition.Gazetteer;
 
 public class IsLocationGazetteer extends BinaryAttribute {
-	public static Gazetteer gazetteer = new Gazetteer("LOCATION", "gazetteer-person.txt") {};
+	public static Gazetteer gazetteer = new Gazetteer("LOCATION", "gazetteer-location.txt") {};
 
 	public IsLocationGazetteer(String surfaceForm){super(gazetteer.contains(surfaceForm));}
 }
--- src/main/java/DataGenerator/Attribute/IsOrganizationGazetteer.java
+++ src/main/java/DataGenerator/Attribute/IsOrganizationGazetteer.java
@@ -4,7 +4,7 @@ import Classification.Attribute.BinaryAttribute;
 import NamedEntityRecognition.Gazetteer;
 
 public class IsOrganizationGazetteer extends BinaryAttribute {
-	public static Gazetteer gazetteer = new Gazetteer("ORGANIZATION", "gazetteer-person.txt") {};
+	public static Gazetteer gazetteer = new Gazetteer("ORGANIZATION", "gazetteer-organization.txt") {};
 
 	public IsOrganizationGazetteer(String surfaceForm){super(gazetteer.contains(surfaceForm));}
 }
